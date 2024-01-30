62173330ad39f2e966f093c2fa3075c813dc0de8
fernandopalafox <ferpalafoxe@gmail.com>
2024-01-30 14:55:12 -0600

    added heatmap plotting utility for stage 1 cost function

--- experiments/tower_defense.jl
+++ experiments/tower_defense.jl
@@ -2,6 +2,7 @@ using GamesVoI
 using BlockArrays
 using LinearAlgebra: norm_sqr, norm
 using Zygote  
+using GLMakie: heatmap!, Figure, Axis, Colorbar, text
 
 """ Nomenclature
     N                         : Number of worlds (=3)
@@ -45,14 +46,77 @@ function solve_r(ps, βs; r_init = [1/3, 1/3, 1/3], iter_limit=50, target_error=
         dKdr = compute_dKdr(r, x, ps, βs, game)
         r_temp = r - α .* dKdr 
         r = project_onto_simplex(r_temp) 
-        println("r_temp = $(round.(r_temp, digits=3)), dKdr = $(round.(dKdr, digits=3)) r = $(round.(r, digits=3))")
         x = compute_stage_2(
             r, ps, βs, game;
             initial_guess=vcat(x, zeros(total_dim(game) - n_players * var_dim))
         )
+        # compute stage 1 cost function for current r and x 
+        K = compute_K(r, x, ps, βs)
+        # println("r_temp = $(round.(r_temp, digits=3)), dKdr = $(round.(dKdr, digits=3)) r = $(round.(r, digits=3)) K = $(round(K, digits=3))")
+        println("r = $(round.(r, digits=3))")
         cur_iter += 1
     end
     return r
+end
+
+"""
+Temp. script to calculate and plot heatmap of Stage 1 cost function 
+"""
+function run_heatmap()
+    ps = [1/3, 1/3, 1/3]
+    βs = [[1, 0.5, 0.5], [0.5, 1, 0.5], [0.5, 0.5, 1]]
+    Ks = calculate_stage_1_heatmap(ps, βs)
+    fig = display_heatmap(ps, Ks)
+    fig
+end
+
+
+"""
+Calculate Stage 1's objective function for all possible values of r.
+
+Inputs: 
+    ps: prior distribution of k worlds for each signal, nx1 vector
+    βs: vector containing P2's cost parameters for each world. vector of nx1 vectors
+    dr: step size for r
+Outputs:
+    Ks: 2D Matrix of stage 1's objective function values for each r in the simplex.
+"""
+function calculate_stage_1_heatmap(ps, βs; dr = 0.1)
+    @assert sum(ps) ≈ 1.0 "Prior distribution ps must be a probability distribution"
+    game, _ = build_stage_2(ps, βs)
+    rs = 0:dr:1
+    Ks = NaN*ones(Float64, Int(1/dr + 1), Int(1/dr + 1))
+    for (i, r1) in enumerate(rs)
+        for (j, r2) in enumerate(rs)
+            if r1 + r2 > 1
+                continue
+            end
+            r3 = 1 - r1 - r2    
+            r = [r1, r2, r3]            
+            x = compute_stage_2(r, ps, βs, game)
+            K = compute_K(r, x, ps, βs)
+            Ks[i, j] = K
+        end
+    end
+
+    return Ks
+end
+
+"""
+Display heatmap of Stage 1's objective function. Assumes number of worlds is 3.
+
+Input: 
+    Ks: 2D Matrix of stage 1's objective function values for each r in the simplex
+Output: 
+    fig: Figure with simplex heatmap
+"""
+function display_heatmap(ps, Ks)
+    rs = 0:1/(size(Ks)[1] - 1):1
+    fig = Figure(size = (600, 400))  
+    ax = Axis(fig[1, 1]; xlabel = "r₁", ylabel = "r₂", aspect = 1)    
+    hmap = heatmap!(ax, rs, rs, Ks)
+    Colorbar(fig[1, 2], hmap; label = "K", width = 15, ticksize = 15, tickalign = 1)
+    fig
 end
 
 """
